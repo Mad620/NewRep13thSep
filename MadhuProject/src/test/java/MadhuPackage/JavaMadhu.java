d18ee1f58b685e06297a0bb8463d48c2720da2e3
Mad620 <rajmadhu456@gmail.com>
2024-09-13 21:22:35 +0530

    12

---
+++ MadhuProject/src/test/java/MadhuPackage/JavaMadhu.java
@@ -0,0 +1,14 @@
+package MadhuPackage;
+
+public class JavaMadhu {
+
+	public static void main(String[] args) {
+
+			System.out.println("Testing for 13th sep");
+			System.out.println("welcom");
+			System.out.println("my new ");
+
+
+	}
+
+}
